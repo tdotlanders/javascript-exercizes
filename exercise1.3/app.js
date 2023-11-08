71df06ae32ec2def34d9bee147a5eb3ea1c590e9
Marco Ventura <venturapm2000@gmail.com>
2023-11-08 19:18:24 +0000

    added exercises

---
+++ exercise1.3/app.js
@@ -0,0 +1,20 @@
+function sum2numbers(a, b) {
+  return a + b;
+}
+
+function sub2numbers(a, b) {
+  return a - b;
+}
+function multiply2numbers(a, b) {
+  return a * b;
+}
+function divide2numbers(a, b) {
+  return a / b;
+}
+const calc = multiply2numbers(5, 2);
+
+console.log(calc);
+
+const sumnumbers = (a, b) => a + b;
+
+console.log(sumnumbers(2, 9));
---
+++ exercise1.4/app.js
@@ -0,0 +1,12 @@
+const num1 = 1;
+
+const calc = (x) => {
+  const num2 = 2;
+
+  return num1 + num2 + x;
+};
+
+console.log(calc(3));
+
+//codewars e codekats têm exercícios para praticar, bom para entrevistas
+// other way-->
